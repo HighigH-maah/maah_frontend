3baf5b2221deb4b9a188e7a45ba0585d5bacf26d
yu4923 <100287621+yu4923@users.noreply.github.com>
2024-01-31 15:32:34 +0900

    test commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Main from "./pages/Main/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HiCardDesignSelect from "./pages/HiCardDesignSelect/HiCardDesignSelect";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Main />}></Route>
+      <Route path="/" element={<HiCardDesignSelect />}></Route>
     </Routes>
   </BrowserRouter>
 );
---
+++ src/pages/HiCardDesignSelect/HiCardDesignSelect.js
@@ -0,0 +1,81 @@
+import './hiCardDesignSelect.css';
+
+function HiCardDesignSelect() {
+  return (
+    <div class="item--zHs">
+  <div class="auto-group-fram-hi5">
+    <p class="item--74y">신규 Hi:Card 신청</p>
+    <p class="item--PYH">카드 디자인을 선택해주세요</p>
+    <div class="group-1000003245-FaV">
+      <div class="auto-group-xpr7-Nf7">
+        <div class="group-1000003019-JHs">
+          <div class="cards-vertical-qYh">
+            <div class="auto-group-dsr5-wLq">
+              {/* <img class="paste-image-here-tG5" src="./assets/paste-image-here-fU1.png"/> */}
+              <div class="shape-9xh">
+              </div>
+              <p class="card-name-Dhf">Card Name</p>
+            </div>
+            <div class="card-description-tYu">card description</div>
+          </div>
+          <p class="the-pink-neH">the Pink</p>
+        </div>
+        <div class="group-1000003020-uD7">
+          <div class="cards-vertical-duo">
+            {/* <img class="shape-BAd" src="REPLACE_IMAGE:I187:699;19:6230"/> */}
+            <p class="card-name-HUZ">Card Name</p>
+            <div class="card-description-MzD">card description</div>
+            <div class="noise-4dj">
+            </div>
+          </div>
+          <p class="the-blue-Lr9">the Blue</p>
+        </div>
+      </div>
+      <div class="group-1000003024-ebw">
+        <div class="cards-vertical-zA1">
+          <div class="auto-group-qysf-hKK">
+            {/* <img class="paste-image-here-dTs" src="./assets/paste-image-here.png"/> */}
+            <div class="shape-Lt5">
+            </div>
+            <p class="card-name-pYM">Card Name</p>
+          </div>
+          <div class="card-description-Kk1">card description</div>
+        </div>
+        <p class="x-boost-SJq">X BOOST</p>
+      </div>
+      <div class="auto-group-6x9w-7fs">
+        <div class="group-1000003022-33j">
+          <div class="cards-vertical-Xjb">
+            {/* <img class="shape-5WD" src="REPLACE_IMAGE:I187:702;19:6230"/> */}
+            <p class="card-name-1em">Card Name</p>
+            <div class="card-description-ip5">card description</div>
+            <div class="noise-CjF">
+            </div>
+          </div>
+          <p class="the-blue-6Zj">the Blue</p>
+        </div>
+        <div class="group-1000003023-Lys">
+          <div class="cards-vertical-tkV">
+            <div class="auto-group-hy1w-p8M">
+              {/* <img class="paste-image-here-N9s" src="./assets/paste-image-here-Bpq.png"/> */}
+              <div class="shape-FzM">
+              </div>
+              <p class="card-name-7Wm">Card Name</p>
+            </div>
+            <div class="card-description-27w">card description</div>
+          </div>
+          <p class="x-boost-uSd">X BOOST</p>
+        </div>
+      </div>
+    </div>
+    <div class="group-1000003168-yBb">
+      <div class="group-1000003161-Vfj">
+        <div class="group-1000003158-ioP">X BOOST 선택</div>
+      </div>
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default HiCardDesignSelect;
