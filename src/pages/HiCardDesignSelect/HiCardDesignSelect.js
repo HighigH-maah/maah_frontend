9703eaab0f100e2afa9c031d84cab3d24f1a2c81
yu4923 <100287621+yu4923@users.noreply.github.com>
2024-01-31 16:54:17 +0900

    Hi:Card Design Select Component

--- src/pages/HiCardDesignSelect/HiCardDesignSelect.js
+++ src/pages/HiCardDesignSelect/HiCardDesignSelect.js
@@ -1,80 +1,72 @@
-import './hiCardDesignSelect.css';
+import styled from 'styled-components';
+import './hicarddesignselect.css';
+import blackVelvet from '../../assets/images/Black_velvet.png';
+import whiteVelvet from '../../assets/images/White_velvet.png';
+
+const SelectDesign = styled.div`
+  background: linear-gradient(180deg, #fffdfd 37.44%, #d7d7d7 100%);
+  padding: 200px 0px 200px 0px;
+  font-family: 'M PLUS 1', sans-serif;
+  text-align: center;
+`;
+
+const MainTitle = styled.div`
+  font-size: 60px;
+  font-weight: bold;
+`;
+
+const SubTitle = styled.div`
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+const CardList = styled.div`
+  margin-top: 30px;
+`;
+
+const CardDesign = styled.div`
+  display: inline-block;
+  width: 300px;
+`;
+
+const CardImageDiv = styled.div`
+  height: 325px;
+`;
+
+const CardImage = styled.img`
+  width: 200px;
+  height: 300px;
+  &:hover {
+    transform: scale(1.12);
+    transition: all 0.2s linear;
+  }
+`;
+
+const CardName = styled.div`
+  font-size: 25px;
+  font-family: 'Iceland', sans-serif;
+`;
 
 function HiCardDesignSelect() {
   return (
-    <div class="item--zHs">
-  <div class="auto-group-fram-hi5">
-    <p class="item--74y">신규 Hi:Card 신청</p>
-    <p class="item--PYH">카드 디자인을 선택해주세요</p>
-    <div class="group-1000003245-FaV">
-      <div class="auto-group-xpr7-Nf7">
-        <div class="group-1000003019-JHs">
-          <div class="cards-vertical-qYh">
-            <div class="auto-group-dsr5-wLq">
-              {/* <img class="paste-image-here-tG5" src="./assets/paste-image-here-fU1.png"/> */}
-              <div class="shape-9xh">
-              </div>
-              <p class="card-name-Dhf">Card Name</p>
-            </div>
-            <div class="card-description-tYu">card description</div>
-          </div>
-          <p class="the-pink-neH">the Pink</p>
-        </div>
-        <div class="group-1000003020-uD7">
-          <div class="cards-vertical-duo">
-            {/* <img class="shape-BAd" src="REPLACE_IMAGE:I187:699;19:6230"/> */}
-            <p class="card-name-HUZ">Card Name</p>
-            <div class="card-description-MzD">card description</div>
-            <div class="noise-4dj">
-            </div>
-          </div>
-          <p class="the-blue-Lr9">the Blue</p>
-        </div>
-      </div>
-      <div class="group-1000003024-ebw">
-        <div class="cards-vertical-zA1">
-          <div class="auto-group-qysf-hKK">
-            {/* <img class="paste-image-here-dTs" src="./assets/paste-image-here.png"/> */}
-            <div class="shape-Lt5">
-            </div>
-            <p class="card-name-pYM">Card Name</p>
-          </div>
-          <div class="card-description-Kk1">card description</div>
-        </div>
-        <p class="x-boost-SJq">X BOOST</p>
-      </div>
-      <div class="auto-group-6x9w-7fs">
-        <div class="group-1000003022-33j">
-          <div class="cards-vertical-Xjb">
-            {/* <img class="shape-5WD" src="REPLACE_IMAGE:I187:702;19:6230"/> */}
-            <p class="card-name-1em">Card Name</p>
-            <div class="card-description-ip5">card description</div>
-            <div class="noise-CjF">
-            </div>
-          </div>
-          <p class="the-blue-6Zj">the Blue</p>
-        </div>
-        <div class="group-1000003023-Lys">
-          <div class="cards-vertical-tkV">
-            <div class="auto-group-hy1w-p8M">
-              {/* <img class="paste-image-here-N9s" src="./assets/paste-image-here-Bpq.png"/> */}
-              <div class="shape-FzM">
-              </div>
-              <p class="card-name-7Wm">Card Name</p>
-            </div>
-            <div class="card-description-27w">card description</div>
-          </div>
-          <p class="x-boost-uSd">X BOOST</p>
-        </div>
-      </div>
-    </div>
-    <div class="group-1000003168-yBb">
-      <div class="group-1000003161-Vfj">
-        <div class="group-1000003158-ioP">X BOOST 선택</div>
-      </div>
-    </div>
-  </div>
-</div>
+    <SelectDesign>
+      <MainTitle>Hi:Card 신청</MainTitle>
+      <SubTitle>마음에 드는 디자인을 골라주세요</SubTitle>
+      <CardList>
+        <CardDesign>
+          <CardImageDiv>
+            <CardImage src={blackVelvet}></CardImage>
+          </CardImageDiv>
+          <CardName>Black Velvet</CardName>
+        </CardDesign>
+        <CardDesign>
+        <CardImageDiv>
+          <CardImage src={whiteVelvet}></CardImage>
+        </CardImageDiv>
+          <CardName>White Velvet</CardName>
+        </CardDesign>
+      </CardList>
+    </SelectDesign>
   );
 }
 
