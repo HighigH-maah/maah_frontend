e3c29bcc1f8504b8965c6274c7aad828c74a4f11
KIM HYEON SEONG <liberatjd@gmail.com>
2024-02-02 11:10:10 +0900

    border-box

--- src/pages/Main/Main2.js
+++ src/pages/Main/Main2.js
@@ -15,11 +15,12 @@ const Mainback = styled.div`
 const BackImage = styled.div`
   background-image: url(${mainBackground});
   background-size: cover;
-  background-repeat: no-repeat; 
+  background-repeat: no-repeat;
   height: 5000px;
 `;
 
 const MainPage1 = styled.div`
+  box-sizing: border-box;
   padding: 13vh 3% 0 3%;
   width: 100%;
   height: 90vh;
@@ -66,7 +67,7 @@ const UnselectBanner = styled.span`
 
 const LoginTitle = styled.div`
   font-size: 4rem;
-  background: linear-gradient(180deg, #000000 37.44%, #8C8A83 100%);
+  background: linear-gradient(180deg, #000000 37.44%, #8c8a83 100%);
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
@@ -74,7 +75,7 @@ const LoginTitle = styled.div`
 
 const LoginWelcome = styled.div`
   font-size: 2rem;
-  background: linear-gradient(180deg, #000000 37.44%, #8C8A83 100%);
+  background: linear-gradient(180deg, #000000 37.44%, #8c8a83 100%);
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
@@ -129,7 +130,7 @@ const LoginButton = styled.div`
     margin: 3rem 0 2rem 0;
     border: 0;
     border-radius: 20px;
-    background: linear-gradient(180deg, #7D796C 0%, #B4AD98 80%);
+    background: linear-gradient(180deg, #7d796c 0%, #b4ad98 80%);
   }
 
   & > div > span {
@@ -143,42 +144,48 @@ function Main2(props) {
     <Mainback>
       <BackImage>
         <MainPage1>
-            <Banner>
-              <EventBanner src={banner}></EventBanner>
-              <BannerPage>
-                <SelectBanner></SelectBanner>
-                <UnselectBanner></UnselectBanner>
-                <UnselectBanner></UnselectBanner>
-                <UnselectBanner></UnselectBanner>
-                <UnselectBanner></UnselectBanner>
-              </BannerPage>
-            </Banner>
+          <Banner>
+            <EventBanner src={banner}></EventBanner>
+            <BannerPage>
+              <SelectBanner></SelectBanner>
+              <UnselectBanner></UnselectBanner>
+              <UnselectBanner></UnselectBanner>
+              <UnselectBanner></UnselectBanner>
+              <UnselectBanner></UnselectBanner>
+            </BannerPage>
+          </Banner>
+          <div>
             <div>
-              <div>
-                <LoginTitle>Login</LoginTitle>
-                <LoginWelcome>Welcome to ma:ah Card</LoginWelcome>
-              </div>
-              <div>
-                <LoginInput type="text" placeholder="ID"></LoginInput>
-                <LoginInput type="password" placeholder="Password"></LoginInput>
-                <SocialLogin>
-                  <div>Or continue with</div>
-                  <SocialButtonBlock>
-                    <SocialButton><img src={google}></img></SocialButton>
-                    <SocialButton><img src={naver}></img></SocialButton>
-                    <SocialButton><img src={kakao}></img></SocialButton>
-                  </SocialButtonBlock>
-                </SocialLogin>
-                <LoginButton>
-                  <button>Log in</button>
-                  <div>
-                    <span>아이디 찾기</span>
-                    &nbsp;|&nbsp;
-                    <span>비밀번호 찾기</span>
-                  </div>
-                </LoginButton>
-              </div>
+              <LoginTitle>Login</LoginTitle>
+              <LoginWelcome>Welcome to ma:ah Card</LoginWelcome>
             </div>
+            <div>
+              <LoginInput type="text" placeholder="ID"></LoginInput>
+              <LoginInput type="password" placeholder="Password"></LoginInput>
+              <SocialLogin>
+                <div>Or continue with</div>
+                <SocialButtonBlock>
+                  <SocialButton>
+                    <img src={google}></img>
+                  </SocialButton>
+                  <SocialButton>
+                    <img src={naver}></img>
+                  </SocialButton>
+                  <SocialButton>
+                    <img src={kakao}></img>
+                  </SocialButton>
+                </SocialButtonBlock>
+              </SocialLogin>
+              <LoginButton>
+                <button>Log in</button>
+                <div>
+                  <span>아이디 찾기</span>
+                  &nbsp;|&nbsp;
+                  <span>비밀번호 찾기</span>
+                </div>
+              </LoginButton>
+            </div>
+          </div>
         </MainPage1>
       </BackImage>
     </Mainback>
