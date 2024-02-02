b83e780d8f8057a82d6e1b2383d52bae1617dbdd
ksweeni <77386337+ksweeni@users.noreply.github.com>
2024-02-02 17:54:43 +0900

    application terms commit

--- src/pages/CardApplicationTerms/CardApplicationTerms.js
+++ src/pages/CardApplicationTerms/CardApplicationTerms.js
@@ -1,93 +1,129 @@
-import styled from 'styled-components';
-import close from '../../assets/images/close.png';
-
-const ModalWrap = styled.div`
-    background-color: rgba(0,0,0,0.3);
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-`;
+import styled from "styled-components";
+import close from "../../assets/images/close.png";
 
 const ModalBody = styled.div`
-    background-color: white;
-    position: absolute;
-    top: calc(50% - 400px);
-    left: calc(50% - 450px);
-    width: 800px;
-    height: 700px;
-    padding: 50px;
-    border-radius: 20px;
+  background-color: white;
+  position: absolute;
+  top: calc(50% - 400px);
+  left: calc(50% - 450px);
+  width: 800px;
+  height: 700px;
+  padding: 50px;
+  border-radius: 20px;
 `;
 
 const ModalClose = styled.img`
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  cursor: pointer;
 `;
 
 const ModalTitle = styled.div`
-    text-align: center;
-    font-size: 25px;
-    font-weight: bold;
-    margin-bottom:30px;
+  text-align: center;
+  font-size: 25px;
+  font-weight: bold;
+  margin-bottom: 30px;
 `;
 
 const ModalSubTitle = styled.div`
-    font-size: 25px;
-    font-weight: bold;
-    margin: 30px 0px 30px 0px;
+  font-size: 25px;
+  font-weight: bold;
+  margin: 30px 0px 10px 0px;
+`;
+
+const ApplicationTermContent = styled.div`
+  font-size: 20px;
+  height: 150px;
+  overflow-y: auto;
+  padding: 10px;
 `;
 
 const ApplicationTerm = styled.div`
+  display: flex;
+  font-size: 20px;
+  padding: 15px 40px 15px 40px;
+  margin: 20px 0 20px 0;
+  border-radius: 40px;
+  background-color: #efefef;
+  justify-content: space-between;
+
+  & > input {
+    width: 22.67px;
+  }
+`;
+
+const ApplicationButton = styled.div`
+  text-align: center;
+
+  & > button {
     font-size: 20px;
+    padding: 15px 40px 15px 40px;
+    border: 0;
+    border-radius: 30px;
+    background-color: black;
+    color: white;
+    cursor: pointer;
+  }
 `;
 
 function CardApplicationTerms(props) {
-    const closeModal = () => {
-        let modal = document.getElementById('cardApplicationTerms');
-        modal.style.visibility = 'hidden';
-    }
-    
-    return (
-        <ModalWrap id='cardApplicationTerms'>
-            <ModalBody>
-                <ModalClose src={close} onClick={closeModal}></ModalClose>
-                <div>
-                    <ModalTitle>약관 및 주요 안내사항</ModalTitle>
-                    <div>
-                        <ModalSubTitle>[필수] 약관 및 주요 안내사항 동의</ModalSubTitle>
-                        <ApplicationTerm>
-                            주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울 예정정
-                        </ApplicationTerm>
-                    </div>
-                </div>
-                <div>
-                    <ModalSubTitle>카드 신청 약관 동의</ModalSubTitle>
-                    <div>
-                        <span>[필수] 약관 및 주요 안내사항 동의</span>
-                        <input type='checkbox'></input>
-                    </div>
-                    <div>
-                        <span>[필수] 개인(신용)정보 동의</span>
-                        <input type='checkbox'></input>
-                    </div>
-                    <div>
-                        <span>[선택] 개인(신용)정보 동의</span>
-                        <input type='checkbox'></input>
-                    </div>
-                    <div>
-                        <span>[선택] 카드 신청/이용을 위한 동의</span>
-                        <input type='checkbox'></input>
-                    </div>
-                    <div>
-                        <button>확인</button>
-                    </div>
-                </div>
-            </ModalBody>
-        </ModalWrap>
-    );
+  const closeModal = () => {
+    let modal = document.getElementById("cardApplicationTerms");
+    modal.style.visibility = "hidden";
+  };
+
+  return (
+    <ModalBody>
+      <ModalClose src={close} onClick={closeModal}></ModalClose>
+      <div>
+        <ModalTitle>약관 및 주요 안내사항</ModalTitle>
+        <div>
+          <ModalSubTitle>[필수] 약관 및 주요 안내사항 동의</ModalSubTitle>
+          <ApplicationTermContent>
+            주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 /
+            약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한
+            내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울
+            예정정 주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드
+            사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한
+            내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울
+            예정정 주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드
+            사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한
+            내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울
+            예정정 주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드
+            사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한
+            내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울
+            예정정 주 마하카드 사 / 약관에 관한 내용으로 채울 예정정 주 마하카드
+            사 / 약관에 관한 내용으로 채울 예정정 주 마하카드 사 / 약관에 관한
+            내용으로 채울 예정정 주 마하카드 사 / 약관에 관한 내용으로 채울
+            예정정
+          </ApplicationTermContent>
+        </div>
+      </div>
+      <div>
+        <ModalSubTitle>카드 신청 약관 동의</ModalSubTitle>
+        <ApplicationTerm>
+          <span>[필수] 약관 및 주요 안내사항 동의</span>
+          <input type="checkbox"></input>
+        </ApplicationTerm>
+        <ApplicationTerm>
+          <span>[필수] 개인(신용)정보 동의</span>
+          <input type="checkbox"></input>
+        </ApplicationTerm>
+        <ApplicationTerm>
+          <span>[선택] 개인(신용)정보 동의</span>
+          <input type="checkbox"></input>
+        </ApplicationTerm>
+        <ApplicationTerm>
+          <span>[선택] 카드 신청/이용을 위한 동의</span>
+          <input type="checkbox"></input>
+        </ApplicationTerm>
+        <ApplicationButton>
+          <button>확인</button>
+        </ApplicationButton>
+      </div>
+    </ModalBody>
+  );
 }
 
 export default CardApplicationTerms;
--- src/pages/HiCardDesignSelect/HiCardDesignSelect.js
+++ src/pages/HiCardDesignSelect/HiCardDesignSelect.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
-import blackVelvet from '../../assets/images/Black_velvet.png';
-import whiteVelvet from '../../assets/images/White_velvet.png';
-import blackBack from '../../assets/images/black-back.png';
-import whiteBack from '../../assets/images/white-back.png';
-import reverse from '../../assets/images/reverse.png';
-import { useState } from 'react';
-import CardApplicationTerms from '../CardApplicationTerms/CardApplicationTerms';
+import styled from "styled-components";
+import blackVelvet from "../../assets/images/Black_velvet.png";
+import whiteVelvet from "../../assets/images/White_velvet.png";
+import blackBack from "../../assets/images/black-back.png";
+import whiteBack from "../../assets/images/white-back.png";
+import reverse from "../../assets/images/reverse.png";
+import { useState } from "react";
+import CardApplicationTerms from "../CardApplicationTerms/CardApplicationTerms";
 
 const SelectDesign = styled.div`
   background: linear-gradient(180deg, #fffdfd 37.44%, #d7d7d7 100%);
@@ -59,7 +59,12 @@ const ReverseButton = styled.button`
   border-radius: 21px;
   border: 0;
   cursor: pointer;
-  background: linear-gradient(0deg, rgba(128, 128, 128, 0.5), rgba(200, 200, 200, 0.5) 45%, rgba(128, 128, 128, 0.5));
+  background: linear-gradient(
+    0deg,
+    rgba(128, 128, 128, 0.5),
+    rgba(200, 200, 200, 0.5) 45%,
+    rgba(128, 128, 128, 0.5)
+  );
   bottom: 70px;
 `;
 
@@ -67,56 +72,94 @@ const ReverseIcon = styled.img`
   margin-top: 5px;
 `;
 
-function HiCardDesignSelect() {
+const ModalWrap = styled.div`
+  background-color: rgba(0, 0, 0, 0.3);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  visibility: hidden;
+`;
 
+function HiCardDesignSelect() {
   const [black, setBlack] = useState(true);
   const [white, setWhite] = useState(true);
 
   const reverseCard = (prop) => {
-    if(prop === "black") {
-      let card = document.getElementById('blackValvet');
-      card.style.transform = 'rotateY(90deg)';
-      setTimeout(function() {
-        card.style.transform = 'rotateY(0deg)';
+    if (prop === "black") {
+      let card = document.getElementById("blackValvet");
+      card.style.transform = "rotateY(90deg)";
+      setTimeout(function () {
+        card.style.transform = "rotateY(0deg)";
         setBlack(!black);
       }, 100);
-    } else if(prop === 'white') {
-      let card = document.getElementById('whiteValvet');
-      card.style.transform = 'rotateY(90deg)';
-      setTimeout(function() {
-        card.style.transform = 'rotateY(0deg)';
+    } else if (prop === "white") {
+      let card = document.getElementById("whiteValvet");
+      card.style.transform = "rotateY(90deg)";
+      setTimeout(function () {
+        card.style.transform = "rotateY(0deg)";
         setWhite(!white);
       }, 100);
     }
-  }
+  };
 
   const applicationTerm = (prop) => {
-    console.log(prop);
-  }
-  
+    let modal = document.getElementById("cardApplicationTerms");
+    console.log(modal);
+    modal.style.visibility = "visible";
+  };
+
   return (
     <>
-    <SelectDesign>
-      <MainTitle>Hi:Card 신청</MainTitle>
-      <SubTitle>마음에 드는 디자인을 골라주세요</SubTitle>
-      <CardList>
-        <CardDesign>
-          <CardImageDiv>
-            <CardImage id='blackValvet' onClick={() => {applicationTerm('black')}} src={black ? blackVelvet : blackBack}></CardImage>
-          </CardImageDiv>
-          <CardName>Black Velvet</CardName>
-          <ReverseButton type='button' onClick={() => {reverseCard('black')}}><ReverseIcon src={reverse}></ReverseIcon></ReverseButton>  
-        </CardDesign>
-        <CardDesign>
-          <CardImageDiv>
-            <CardImage id='whiteValvet' onClick={() => {applicationTerm('white')}} src={white ? whiteVelvet : whiteBack}></CardImage>
-          </CardImageDiv>
-          <CardName>White Velvet</CardName>
-          <ReverseButton type='button' onClick={() => {reverseCard('white')}}><ReverseIcon src={reverse}></ReverseIcon></ReverseButton>
-        </CardDesign>
-      </CardList>
-    </SelectDesign>
-    <CardApplicationTerms></CardApplicationTerms>
+      <SelectDesign>
+        <MainTitle>Hi:Card 신청</MainTitle>
+        <SubTitle>마음에 드는 디자인을 골라주세요</SubTitle>
+        <CardList>
+          <CardDesign>
+            <CardImageDiv>
+              <CardImage
+                id="blackValvet"
+                onClick={() => {
+                  applicationTerm("black");
+                }}
+                src={black ? blackVelvet : blackBack}
+              ></CardImage>
+            </CardImageDiv>
+            <CardName>Black Velvet</CardName>
+            <ReverseButton
+              type="button"
+              onClick={() => {
+                reverseCard("black");
+              }}
+            >
+              <ReverseIcon src={reverse}></ReverseIcon>
+            </ReverseButton>
+          </CardDesign>
+          <CardDesign>
+            <CardImageDiv>
+              <CardImage
+                id="whiteValvet"
+                onClick={() => {
+                  applicationTerm("white");
+                }}
+                src={white ? whiteVelvet : whiteBack}
+              ></CardImage>
+            </CardImageDiv>
+            <CardName>White Velvet</CardName>
+            <ReverseButton
+              type="button"
+              onClick={() => {
+                reverseCard("white");
+              }}
+            >
+              <ReverseIcon src={reverse}></ReverseIcon>
+            </ReverseButton>
+          </CardDesign>
+        </CardList>
+      </SelectDesign>
+      <ModalWrap id="cardApplicationTerms">
+        <CardApplicationTerms></CardApplicationTerms>
+      </ModalWrap>
     </>
   );
 }
