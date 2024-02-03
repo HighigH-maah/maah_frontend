9fe75f9695fc68b7df8ce1babff77ef18057b0aa
Jieun Park <80210564+wldms819@users.noreply.github.com>
2024-02-03 17:29:28 +0900

    hicard detail front fin

--- src/components/HiCard/HiCard.js
+++ src/components/HiCard/HiCard.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import cardImg from "./HICard.png";
+import cardImg from "./icon/HICard.png";
+import cardGradeImg from "./icon/bronze.png";
+import phoneImg from "./icon/phone.png";
+import trafficImg from "./icon/traffic.png";
+import foodImg from "./icon/food.png";
+import utilitiesImg from "./icon/utilities.png";
+import hospitalImg from "./icon/hospital.png";
+import leisureImg from "./icon/leisure.png";
+import shoppingImg from "./icon/shopping.png";
+import studyImg from "./icon/study.png";
+import oilingImg from "./icon/oiling.png";
+import cultureImg from "./icon/culture.png";
+import airlineImg from "./icon/airline.png";
+import travelImg from "./icon/travel.png";
+
+import chevronDown from "./icon/chevronDown.png";
+import chevronUp from "./icon/chevronUp.png";
 
 const HiCardDiv = styled.div`
   display: flex;
@@ -15,7 +31,7 @@ const HiCardDetailOuterDiv = styled.div`
   box-sizing: border-box;
   padding: 5rem 10rem 5rem 10rem;
   width: 1500px;
-  height: 700px;
+  //height: 700px;
   align-items: center;
   display: flex;
   flex-direction: column;
@@ -51,16 +67,10 @@ const HiCardDetailRight = styled.div`
   flex-shrink: 0;
   display: flex;
   flex-direction: column;
-`;
-
-const HiCardName = styled.div`
-  margin-bottom: 2rem;
-  display: flex;
-  flex-direction: column;
 
   .name1 {
-    margin-bottom: 1rem;
-    font-size: 4rem;
+    margin-bottom: 0rem;
+    font-size: 3rem;
     font-weight: 400;
     line-height: 0.5;
     color: #000000;
@@ -80,86 +90,298 @@ const HiCardName = styled.div`
   }
 `;
 
-const HiCardDetails = styled.div`
+const HiCardMileage = styled.div`
+  margin: 0rem 0rem 2rem 0rem;
   width: 100%;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Box1 = styled.div`
-  width: 100%;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Mframe = styled.div`
-  margin: 0rem 1.6rem 2rem 0rem;
-  width: calc(100% - 1.6rem);
   display: flex;
   align-items: center;
   flex-shrink: 0;
 
-  .mileageTitle {
-    margin-right: 10.6rem;
+  .title {
+    margin-right: 10rem;
     display: flex;
-    font-size: 3.6rem;
+    font-size: 2.5rem;
     font-weight: 400;
-    line-height: 0.6111111111;
     letter-spacing: -0.018rem;
     color: #000000;
-    font-family: Iceland, 'Source Sans Pro';
+    font-family: Iceland,"Source Sans Pro";
     white-space: nowrap;
     flex-shrink: 0;
 }
   }
 
-  .mileage{
+  .mileage {
     display: flex;
-    font-size: 3.6rem;
+    font-size: 2.5rem;
     font-weight: 400;
-    line-height: 0.6111111111;
-    letter-spacing: -0.018rem;
     color: #000000;
-    font-family: Iceland, 'Source Sans Pro';
+    font-family: Iceland,"Source Sans Pro";
     white-space: nowrap;
     flex-shrink: 0;
 }
   }
 `;
 
-const GFrame = styled.div`
-margin-right: 0.8rem;
-width: calc(100% - 0.8rem);
-display: flex;
-align-items: center;
-flex-shrink: 0;
-}
-
-div {
-  margin-right: 3rem;
+const HiCardGrade = styled.div`
+  margin-bottom: 2rem;
+  width: 100%;
   display: flex;
-  font-size: 3.6rem;
-  font-weight: 400;
-  line-height: 0.6111111111;
-  letter-spacing: -0.018rem;
-  color: #000000;
-  font-family: Iceland, 'Source Sans Pro';
-  white-space: nowrap;
+  align-items: center;
   flex-shrink: 0;
-}
 
-img {
-  width: 11rem;
-  height: 8rem;
-  object-fit: cover;
-  vertical-align: top;
+  div {
+    margin-right: 4rem;
+    display: flex;
+    font-size: 2.5rem;
+    font-weight: 400;
+    color: #000000;
+    font-family: Iceland, "Source Sans Pro";
+    white-space: nowrap;
+    flex-shrink: 0;
+  }
+
+  img {
+    height: 7rem;
+    object-fit: cover;
+    vertical-align: top;
+    flex-shrink: 0;
+  }
+`;
+
+const HiCardBtn = styled.div`
+  width: 100%;
+  align-items: flex-end;
+  display: flex;
+  flex-direction: column;
   flex-shrink: 0;
-}
+
+  button {
+    width: 100%;
+    height: 5rem;
+    text-align: center;
+    font-size: 1.8rem;
+    font-weight: 400;
+    color: #ffffff;
+    font-family: Iceland, "Source Sans Pro";
+    white-space: nowrap;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(
+      180deg,
+      rgba(188, 185, 179, 0.8539999723) -0%,
+      rgba(177, 172, 148, 0.9720500112) 34%,
+      rgba(130, 127, 116, 0.8700000048) 98%
+    );
+    border-radius: 0.5rem;
+    flex-shrink: 0;
+  }
+
+  div {
+    margin: 0rem 0rem 0rem 15rem;
+    display: flex;
+    align-items: center;
+    flex-shrink: 0;
+  }
+
+  .accountChange {
+    margin-right: 3rem;
+    text-align: center;
+    font-size: 1.3rem;
+    font-weight: 400;
+    color: #000000;
+    font-family: Iceland, "Source Sans Pro";
+    white-space: nowrap;
+    flex-shrink: 0;
+  }
+
+  .virtualCardApply {
+    text-align: center;
+    font-size: 1.3rem;
+    font-weight: 400;
+    color: #000000;
+    font-family: Iceland, "Source Sans Pro";
+    white-space: nowrap;
+    flex-shrink: 0;
+  }
+`;
+
+const HiCardLimit = styled.div`
+  margin: 5rem 25rem 0rem 25rem;
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+
+  .cardLimit {
+    margin-right: 15rem;
+    display: flex;
+    font-size: 1.6rem;
+    font-weight: 400;
+    color: #000000;
+    font-family: Iceland, "Source Sans Pro";
+    white-space: nowrap;
+    flex-shrink: 0;
+  }
+
+  .cardType {
+    display: flex;
+    font-size: 1.6rem;
+    font-weight: 400;
+    color: #000000;
+    font-family: Iceland, "Source Sans Pro";
+    white-space: nowrap;
+    flex-shrink: 0;
+  }
+`;
+
+const HiCardBenefits = styled.div`
+  margin: 1rem 0rem;
+  box-sizing: border-box;
+  padding: 3rem;
+  width: 1500px;
+  height: 10rem;
+  display: flex;
+  align-items: center;
+  background: linear-gradient(180deg, #ffffff -0%, #f0f0f0 100%);
+  border-radius: 1rem;
+  cursor: pointer;
+`;
+
+const HiCardBenefitsContent = styled.div`
+  box-sizing: border-box;
+  padding: 1rem;
+  width: 1500px;
+  display: flex;
+  position: relative;
+  bottom: 55px;
+  align-items: center;
+  background: linear-gradient(180deg, #f0f0f0 -0%, #ffffff 100%);
+  border-radius: 1rem;
+`;
+
+const Category = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+
+  .icon {
+    margin: 0rem 5rem;
+    width: 5rem;
+    height: 5rem;
+    object-fit: contain;
+    vertical-align: top;
+    flex-shrink: 0;
+  }
+
+  .catagory {
+    margin-right: 10rem;
+    font-size: 2.5rem;
+    font-weight: 400;
+    color: #2d2d2d;
+    font-family: Iceland, "Source Sans Pro";
+    white-space: nowrap;
+    flex-shrink: 0;
+  }
+
+  .catagoryBenefits {
+    margin-right: 20rem;
+    font-size: 2.5rem;
+    font-weight: 400;
+    color: #6d6d6d;
+    font-family: Iceland, "Source Sans Pro";
+    white-space: nowrap;
+    flex-shrink: 0;
+  }
+
+  .chevronIcon {
+    margin-left: 10rem;
+    width: 2rem;
+    height: 2rem;
+    object-fit: contain;
+    vertical-align: top;
+    flex-shrink: 0;
+  }
 `;
 
 function HiCard(props) {
+  const [openAccordions, setOpenAccordions] = useState([]);
+
+  const toggleAccordion = (index) => {
+    if (openAccordions.includes(index)) {
+      // If accordion is open, close it
+      setOpenAccordions(openAccordions.filter((item) => item !== index));
+    } else {
+      // If accordion is closed, open it
+      setOpenAccordions([...openAccordions, index]);
+    }
+  };
+
+  const accordions = [
+    {
+      image: phoneImg,
+      category: "통신",
+      benefit: "통신요금 10% 할인",
+    },
+    {
+      image: trafficImg,
+      category: "교통",
+      benefit: "대중교통 10% 할인",
+    },
+    {
+      image: foodImg,
+      category: "푸드",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: utilitiesImg,
+      category: "공금",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: hospitalImg,
+      category: "병원",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: leisureImg,
+      category: "레저",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: shoppingImg,
+      category: "쇼핑",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: studyImg,
+      category: "교육",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: oilingImg,
+      category: "주유",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: cultureImg,
+      category: "문화",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: airlineImg,
+      category: "항공",
+      benefit: "마켓컬리 10% 할인",
+    },
+    {
+      image: travelImg,
+      category: "여행",
+      benefit: "마켓컬리 10% 할인",
+    },
+    // Add more accordions as needed
+  ];
+
   return (
     <HiCardDiv>
       <HiCardDetailOuterDiv>
@@ -168,25 +390,61 @@ function HiCard(props) {
             <img src={cardImg} alt="하이카드 이미지"></img>
           </HiCardDetailLeft>
           <HiCardDetailRight>
-            <HiCardName>
-              <p className="name1">Hi:Card</p>
-              <p className="name2">하이카드</p>
-            </HiCardName>
-            {/* <HiCardDetails>
-              <Box1>
-                <Mframe>
-                  <div className="mileageTitle">Hi:Mileage</div>
-                  <div className="mileage">4500M</div>
-                </Mframe>
-                <GFrame>
-                  <div>Hi:Credit Grade</div>
-                  <img src="" alt="하이카드 등급 이미지"></img>
-                </GFrame>
-              </Box1>
-            </HiCardDetails> */}
+            <p className="name1">Hi:Card</p>
+            <p className="name2">하이카드</p>
+
+            <HiCardMileage>
+              <div className="title">Hi:Mileage</div>
+              <div className="mileage">4500M</div>
+            </HiCardMileage>
+
+            <HiCardGrade>
+              <div>Hi:Credit Grade</div>
+              <img src={cardGradeImg} alt="하이카드 등급 이미지"></img>
+            </HiCardGrade>
+
+            <HiCardBtn>
+              <button>나의 결제 이력</button>
+              <div>
+                <p className="accountChange">연결계좌변경</p>
+                <p className="virtualCardApply">가상 카드 발급신청</p>
+              </div>
+            </HiCardBtn>
           </HiCardDetailRight>
         </HiCardDetailInnerDiv>
+        <HiCardLimit>
+          <div className="cardLimit">최대한도 100만원</div>
+          <div className="cardType">mastercard</div>
+        </HiCardLimit>
       </HiCardDetailOuterDiv>
+
+      {accordions.map((accordion, index) => (
+        <div key={index}>
+          <HiCardBenefits onClick={() => toggleAccordion(index)}>
+            <Category>
+              <img
+                className="icon"
+                src={accordion.image}
+                alt="카테고리 아이콘 이미지"
+              ></img>
+              <p className="catagory">{accordion.category}</p>
+              <p className="catagoryBenefits">{accordion.benefit}</p>
+              <img
+                className="chevronIcon"
+                src={openAccordions.includes(index) ? chevronUp : chevronDown}
+                alt="chevronIcon"
+              ></img>
+            </Category>
+          </HiCardBenefits>
+          {openAccordions.includes(index) && (
+            <HiCardBenefitsContent>
+              {/* Additional Content to show when accordion is open */}
+              <p>{accordion.benefit}</p>
+              {/* ... more content */}
+            </HiCardBenefitsContent>
+          )}
+        </div>
+      ))}
     </HiCardDiv>
   );
 }
