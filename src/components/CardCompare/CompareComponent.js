d9e4e96f0d9c3620cbe5160845aad074f033ce2e
ksweeni <77386337+ksweeni@users.noreply.github.com>
2024-02-05 16:30:44 +0900

    Feat: 카드 비교 Styling

--- src/components/CardCompare/CompareComponent.js
+++ src/components/CardCompare/CompareComponent.js
@@ -1,15 +1,22 @@
 import styled from "styled-components";
+import circle from "../../assets/images/circle.png";
+import blackVelvet from "../../assets/images/Black_velvet.png";
 export const SelectDiv = styled.div`
-  background-color: gray;
+  background-color: lightgray;
+  position: relative;
+  height: 380px;
+  margin: 0 auto;
   color: black;
   display: flex;
+  width: 1144px;
   gap: 2rem;
   border-radius: 30px;
   flex-direction: column;
+  top: 5rem;
 `;
 
 export const Company = styled.div`
-  display: flex;
+  gap: 1rem;
   font-weight: bolder;
   font-size: 2rem;
   display: flex;
@@ -19,33 +26,72 @@ export const Company = styled.div`
 
 export const CardExample = styled.button`
   width: 250px;
+  border: none;
   color: black;
   height: 40px;
   border-radius: 50px;
   box-shadow: 0px 4px 4px #0000004d;
   font-weight: bolder;
   cursor: pointer;
+  transition: background 0.3s ease;
+
+  &:hover {
+    background: black;
+    color: white;
+  }
 `;
 
+export const BtnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  & > span {
+    font-size: 1rem;
+  }
+`;
+
+export const Btn = ({ image, color, title }) => {
+  let imgWidth = 48;
+  let imgHeight = 48;
+
+  if (title === "현대카드") {
+    imgWidth = 60;
+    imgHeight = 12;
+  }
+
+  return (
+    <BtnWrapper>
+      <CompanyBtn color={color}>
+        <img src={image} alt={title} width={imgWidth} height={imgHeight} />
+      </CompanyBtn>
+      <span>{title}</span>
+    </BtnWrapper>
+  );
+};
+
 export const CompanyBtn = styled.button`
+  transition: box-shadow 0.3s ease;
   width: 80px;
+  display: flex;
+  border: none;
+  cursor: pointer;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   height: 80px;
   color: black;
   border-radius: 20px;
-  background-color: #feffce;
+  background-color: ${(props) => props.color || "#feffce"};
+
+  &:hover {
+    box-shadow: 0px 4px 4px gray;
+  }
 `;
 export const BtnDiv = styled.div`
   display: flex;
   gap: 1rem;
-`;
-
-export const MaahBtn = styled.div`
-  width: 80px;
-  font-size: 1rem;
-  height: 80px;
-  color: black;
-  border-radius: 20px;
-  background-color: #feffce;
 `;
 
 export const CategoryDiv = styled.div`
@@ -55,6 +101,7 @@ export const CategoryDiv = styled.div`
 
 export const CategoryBtn = styled.div`
   width: 129px;
+  background-color: white;
   cursor: pointer;
   border-radius: 50px;
   display: flex;
@@ -63,12 +110,134 @@ export const CategoryBtn = styled.div`
   box-shadow: 0px 4px 4px #0000004d;
   align-items: center;
   justify-content: center;
+  transition: background 0.3s ease;
+
+  &:hover {
+    background: black;
+    color: white;
+  }
 `;
 
 export const FirstDiv = styled.div`
   display: flex;
+  flex-direction: row;
+  width: 69rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 1rem;
 `;
 
 export const SecondDiv = styled.div`
   display: flex;
+  flex-direction: row;
+  width: 69rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ListDiv = styled.div`
+  display: flex;
+  gap: 1rem;
+  position: relative;
+  top: 10rem;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CardList = styled.div`
+  width: 1144px;
+  height: 324px;
+  gap: 2rem;
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  position: relative;
+  margin: 0 auto;
+  border-radius: 10px;
+`;
+
+const ImageOverlay = styled.div`
+  position: relative;
+  width: 200px;
+  height: 200px;
+`;
+const CardImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 120px;
+  height: 180px;
+`;
+export const ExampleImage = () => {
+  return (
+    <ImageOverlay>
+      <img src={circle} alt="Circle" width="200" height="200" />
+      <CardImage src={blackVelvet} alt="Black Velvet" />
+    </ImageOverlay>
+  );
+};
+
+export const CardImageDiv = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 1rem;
+`;
+
+export const CardDetailDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2rem;
+  justify-content: center;
+`;
+
+export const CardEvent = styled.div`
+  background-color: #e6e6e6;
+  border-radius: 20px;
+  font-weight: bolder;
+  font-size: 1rem;
+  padding: 10px;
+`;
+
+export const Benefit = styled.div`
+  font-weight: bolder;
+`;
+
+export const BenefitDiv = styled.div`
+  gap: 3rem;
+  display: flex;
+  align-items: center;
+`;
+
+export const ConditionDiv = styled.div`
+  display: flex;
+  color: lightgray;
+  gap: 12.5rem;
+`;
+
+export const CardTitle = styled.div`
+  font-weight: bolder;
+  font-size: 2rem;
+`;
+
+export const DetailBtnDiv = styled.div`
+  display: flex;
+
+  & > button {
+    font-weight: bolder;
+    border: none;
+    border-radius: 5px;
+    margin-right: 2rem;
+    margin-top: 2rem;
+    width: 199px;
+    height: 56px;
+    cursor: pointer;
+    transition: box-shadow 0.3s ease;
+  }
+
+  & > button:hover {
+    box-shadow: 0px 4px 4px gray;
+  }
 `;
--- src/pages/CardCompare/CardCompare.js
+++ src/pages/CardCompare/CardCompare.js
@@ -2,49 +2,60 @@ import React from "react";
 import kookmin from "../../assets/images/kookmin.png";
 import shinhan from "../../assets/images/shinhan.png";
 import hyundai from "../../assets/images/hyundai.png";
+import samsung from "../../assets/images/samsung.png";
+import lotte from "../../assets/images/lotte.png";
+import maahSmall from "../../assets/images/Maah_small.png";
 import { BackImage, Mainback } from "../../components/Main/MainComponent";
+
 import {
+  Benefit,
+  BenefitDiv,
+  Btn,
   BtnDiv,
+  CardDetailDiv,
+  CardEvent,
   CardExample,
+  CardImageDiv,
+  CardList,
+  CardTitle,
   CategoryBtn,
   CategoryDiv,
   Company,
-  CompanyBtn,
+  ConditionDiv,
+  DetailBtnDiv,
+  ExampleImage,
   FirstDiv,
-  MaahBtn,
+  ListDiv,
   SecondDiv,
   SelectDiv,
 } from "../../components/CardCompare/CompareComponent";
+import HeaderLogoutBtn from "../../components/Header/HeaderLogoutBtn";
+import Footer from "../../components/Footer/Footer";
 
 function CardCompare(props) {
   return (
     <Mainback>
       <BackImage>
+        <HeaderLogoutBtn></HeaderLogoutBtn>
         <SelectDiv>
           <FirstDiv>
             <Company>
               <span>Card Company</span>
               <BtnDiv>
-                <CompanyBtn>
-                  <img src={kookmin} />
-                  KB국민카드
-                </CompanyBtn>
-                <CompanyBtn>
-                  <img src={shinhan} />
-                  신한카드
-                </CompanyBtn>
-                <CompanyBtn>
-                  {" "}
-                  <img src={hyundai} />
-                  현대카드
-                </CompanyBtn>
-                <CompanyBtn>하나은행</CompanyBtn>
-                <CompanyBtn>하나은행</CompanyBtn>
+                <Btn image={shinhan} color={"#D6EBFF"} title={"신한카드"} />
+                <Btn image={kookmin} color={"#FEFFCE"} title={"국민카드"} />
+                <Btn image={hyundai} color={"#E6E6E6"} title={"현대카드"} />
+                <Btn image={samsung} color={"#ffffff"} title={"삼성카드"} />
+                <Btn image={lotte} color={"#ffecec"} title={"롯데카드"} />
               </BtnDiv>
             </Company>
             <Company>
               <span>Only Ma:ah</span>
-              <MaahBtn>Ma:ah 카드 모아보기</MaahBtn>
+              <Btn
+                image={maahSmall}
+                color={"#E6E6E6"}
+                title={"Ma:ah 카드 모아보기"}
+              />
             </Company>
             <Company>
               <span>Category</span>
@@ -68,11 +79,79 @@ function CardCompare(props) {
                 <CardExample>하나은행</CardExample>
                 <CardExample>하나은행</CardExample>
                 <CardExample>하나은행</CardExample>
-                <CardExample>하나은행</CardExample>
               </BtnDiv>
             </Company>
           </SecondDiv>
         </SelectDiv>
+
+        <ListDiv>
+          <CardList>
+            <CardImageDiv>
+              <ExampleImage></ExampleImage>
+            </CardImageDiv>
+            <CardDetailDiv>
+              <CardTitle>마하카드</CardTitle>
+              <CardEvent>신규회원 연회비 캐쉬백 이벤트</CardEvent>
+              <BenefitDiv>
+                <Benefit>업종별 0.5~3% 적립</Benefit>|
+                <Benefit>업종별 0.5~3% 적립</Benefit>|
+                <Benefit>업종별 0.5~3% 적립</Benefit>
+              </BenefitDiv>
+              <ConditionDiv>
+                <p>국내 전용 30,000원/해외겸용 30,000원</p>
+                <p>전월 실적 50만원 이상</p>
+              </ConditionDiv>
+            </CardDetailDiv>
+            <DetailBtnDiv>
+              <button>자세히 보기</button>
+            </DetailBtnDiv>
+          </CardList>
+
+          <CardList>
+            <CardImageDiv>
+              <ExampleImage></ExampleImage>
+            </CardImageDiv>
+            <CardDetailDiv>
+              <CardTitle>마하카드</CardTitle>
+              <CardEvent>신규회원 연회비 캐쉬백 이벤트</CardEvent>
+              <BenefitDiv>
+                <Benefit>업종별 0.5~3% 적립</Benefit>|
+                <Benefit>업종별 0.5~3% 적립</Benefit>|
+                <Benefit>업종별 0.5~3% 적립</Benefit>
+              </BenefitDiv>
+              <ConditionDiv>
+                <p>국내 전용 30,000원/해외겸용 30,000원</p>
+                <p>전월 실적 50만원 이상</p>
+              </ConditionDiv>
+            </CardDetailDiv>
+            <DetailBtnDiv>
+              <button>자세히 보기</button>
+            </DetailBtnDiv>
+          </CardList>
+
+          <CardList>
+            <CardImageDiv>
+              <ExampleImage></ExampleImage>
+            </CardImageDiv>
+            <CardDetailDiv>
+              <CardTitle>마하카드</CardTitle>
+              <CardEvent>신규회원 연회비 캐쉬백 이벤트</CardEvent>
+              <BenefitDiv>
+                <Benefit>업종별 0.5~3% 적립</Benefit>|
+                <Benefit>업종별 0.5~3% 적립</Benefit>|
+                <Benefit>업종별 0.5~3% 적립</Benefit>
+              </BenefitDiv>
+              <ConditionDiv>
+                <p>국내 전용 30,000원/해외겸용 30,000원</p>
+                <p>전월 실적 50만원 이상</p>
+              </ConditionDiv>
+            </CardDetailDiv>
+            <DetailBtnDiv>
+              <button>자세히 보기</button>
+            </DetailBtnDiv>
+          </CardList>
+        </ListDiv>
+        <Footer position="relative" top="40rem"></Footer>
       </BackImage>
     </Mainback>
   );
