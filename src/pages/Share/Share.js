0e27265c6e918cf8adaddc3c29d9c9f2f682ab3c
KIM HYEON SEONG <liberatjd@gmail.com>
2024-02-05 15:51:45 +0900

    axios example

--- src/pages/Share/Share.js
+++ src/pages/Share/Share.js
@@ -1,145 +1,181 @@
 import React from "react";
 import "./share.css";
+import styled from "styled-components";
+import axios from "axios";
+
+const ShareBack = styled.div`
+  width: 100%;
+  height: auto;
+  position: relative;
+  background: linear-gradient(
+    355.98deg,
+    #181048 99.92%,
+    #0e0831 99.93%,
+    #1b463b 200%
+  );
+  background-size: auto 100%;
+`;
+
+const ShareMain = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  left: 2.4889rem;
+  top: 28rem;
+`;
+
+axios
+  .post("/member.do", {
+    member_id: "user3",
+  })
+  .then(function (res) {
+    console.log(res.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 function Share(props) {
   return (
-    <div class="share-8XF">
-      <div class="main-LK7">
-        <div class="group-1000003058-UAR">
-          <div class="auto-group-6kim-yss">
-            <p class="ma-ah-share-tjw">Ma:ah Share</p>
-            <p class="hi-card-name-1pZ">Hi:Card Name</p>
+    <ShareBack>
+      <ShareMain>
+        <div className="group-1000003058-UAR">
+          <div className="auto-group-6kim-yss">
+            <p className="ma-ah-share-tjw">Ma:ah Share</p>
+            <p className="hi-card-name-1pZ">Hi:Card Name</p>
           </div>
-          <div class="auto-group-qpdb-jEm">
-            {/* <img class="right-Fyo" src="REPLACE_IMAGE:42:1680" />
-            <img class="left-zwP" src="REPLACE_IMAGE:42:1679" /> */}
-            <div class="frame-1000005675-uHf">
-              <div class="learn-more-yoK">Learn More</div>
-              {/* <img class="vector-7-WoF" src="REPLACE_IMAGE:13:2167" /> */}
+          <div className="auto-group-qpdb-jEm">
+            {/* <img className="right-Fyo" src="REPLACE_IMAGE:42:1680" />
+            <img className="left-zwP" src="REPLACE_IMAGE:42:1679" /> */}
+            <div className="frame-1000005675-uHf">
+              <div className="learn-more-yoK">Learn More</div>
+              {/* <img className="vector-7-WoF" src="REPLACE_IMAGE:13:2167" /> */}
             </div>
-            <div class="group-1000003024-4iH">
-              <p class="x-boost-y4Z">X BOOST</p>
-              <div class="cards-vertical-34R">
-                <div class="auto-group-v3ft-8rZ">
+            <div className="group-1000003024-4iH">
+              <p className="x-boost-y4Z">X BOOST</p>
+              <div className="cards-vertical-34R">
+                <div className="auto-group-v3ft-8rZ">
                   {/* <img
-                    class="paste-image-here-Gxm"
+                    className="paste-image-here-Gxm"
                     src="./assets/paste-image-here-RLR.png"
                   /> */}
-                  <div class="shape-CbX"></div>
-                  <p class="card-name-VKj">Card Name</p>
+                  <div className="shape-CbX"></div>
+                  <p className="card-name-VKj">Card Name</p>
                 </div>
-                <div class="card-description-zXP">card description</div>
+                <div className="card-description-zXP">card description</div>
               </div>
             </div>
-            {/* <img class="group-1000003025-3Em" src="REPLACE_IMAGE:13:2158" /> */}
+            {/* <img className="group-1000003025-3Em" src="REPLACE_IMAGE:13:2158" /> */}
           </div>
         </div>
-        <div class="auto-group-wqkb-Zyo">
-          {/* <img class="star-img-JwP" src="REPLACE_IMAGE:13:2329" /> */}
-          <div class="auto-group-rnk7-Snh">
-            <div class="group-1000003071-Cms">
-              <div class="send-button-KLh">
-                <div class="send-cqb">Card</div>
-                {/* <img class="group-221-3R7" src="REPLACE_IMAGE:I48:1840;2:8" /> */}
+        <div className="auto-group-wqkb-Zyo">
+          {/* <img className="star-img-JwP" src="REPLACE_IMAGE:13:2329" /> */}
+          <div className="auto-group-rnk7-Snh">
+            <div className="group-1000003071-Cms">
+              <div className="send-button-KLh">
+                <div className="send-cqb">Card</div>
+                {/* <img className="group-221-3R7" src="REPLACE_IMAGE:I48:1840;2:8" /> */}
               </div>
-              <div class="group-1000003065-KdX">
-                <p class="by-card-fhP">By:Card</p>
-                <div class="group-1000003064-MKK">
-                  <div class="group-1000003063-6Gu">
-                    <div class="group-1000003060-E8D">
-                      <div class="group-1000003028-m89">
-                        <p class="the-blue-WbX">the Blue</p>
-                        <div class="cards-vertical-ps7">
+              <div className="group-1000003065-KdX">
+                <p className="by-card-fhP">By:Card</p>
+                <div className="group-1000003064-MKK">
+                  <div className="group-1000003063-6Gu">
+                    <div className="group-1000003060-E8D">
+                      <div className="group-1000003028-m89">
+                        <p className="the-blue-WbX">the Blue</p>
+                        <div className="cards-vertical-ps7">
                           {/* <img
-                            class="shape-aLV"
+                            className="shape-aLV"
                             src="REPLACE_IMAGE:I42:1708;19:6230"
                           /> */}
-                          <p class="card-name-hAD">Card Name</p>
-                          <div class="card-description-N1T">
+                          <p className="card-name-hAD">Card Name</p>
+                          <div className="card-description-N1T">
                             card description
                           </div>
-                          <div class="noise-eDs"></div>
+                          <div className="noise-eDs"></div>
                         </div>
                       </div>
-                      <div class="frame-23-Wms">
-                        <p class="learn-more-oW5">Learn More</p>
-                        {/* <img class="vector-7-wcH" src="REPLACE_IMAGE:42:1727" /> */}
+                      <div className="frame-23-Wms">
+                        <p className="learn-more-oW5">Learn More</p>
+                        {/* <img className="vector-7-wcH" src="REPLACE_IMAGE:42:1727" /> */}
                       </div>
                     </div>
-                    <div class="group-1000003061-ouP">
-                      <div class="group-1000003028-xXP">
-                        <p class="the-blue-WJ1">the Blue</p>
-                        <div class="cards-vertical-nFX">
+                    <div className="group-1000003061-ouP">
+                      <div className="group-1000003028-xXP">
+                        <p className="the-blue-WJ1">the Blue</p>
+                        <div className="cards-vertical-nFX">
                           {/* <img
-                            class="shape-vcd"
+                            className="shape-vcd"
                             src="REPLACE_IMAGE:I42:1731;19:6230"
                           /> */}
-                          <p class="card-name-GRb">Card Name</p>
-                          <div class="card-description-Z9o">
+                          <p className="card-name-GRb">Card Name</p>
+                          <div className="card-description-Z9o">
                             card description
                           </div>
-                          <div class="noise-EFw"></div>
+                          <div className="noise-EFw"></div>
                         </div>
                       </div>
-                      <div class="frame-23-tLV">
-                        <p class="learn-more-NmT">Learn More</p>
-                        {/* <img class="vector-7-iKX" src="REPLACE_IMAGE:42:1735" /> */}
+                      <div className="frame-23-tLV">
+                        <p className="learn-more-NmT">Learn More</p>
+                        {/* <img className="vector-7-iKX" src="REPLACE_IMAGE:42:1735" /> */}
                       </div>
                     </div>
-                    <div class="group-1000003062-p7f">
-                      <div class="group-1000003250-vRb">
-                        <p class="the-blue-5ZP">the Blue</p>
-                        <div class="auto-group-itay-zgM">
-                          <div class="cards-vertical-kfX">
+                    <div className="group-1000003062-p7f">
+                      <div className="group-1000003250-vRb">
+                        <p className="the-blue-5ZP">the Blue</p>
+                        <div className="auto-group-itay-zgM">
+                          <div className="cards-vertical-kfX">
                             {/* <img
-                              class="shape-tWq"
+                              className="shape-tWq"
                               src="REPLACE_IMAGE:I42:1742;19:6230"
                             /> */}
                           </div>
-                          <div class="frame-1000005679-z45">
-                            <div class="frame-1000005677-RfB">
+                          <div className="frame-1000005679-z45">
+                            <div className="frame-1000005677-RfB">
                               {/* <img
-                                class="cart-pz9"
+                                className="cart-pz9"
                                 src="REPLACE_IMAGE:48:1869"
                               /> */}
-                              <div class="item-10--KR7">
+                              <div className="item-10--KR7">
                                 마트, 편의점 10% 할인
                               </div>
                             </div>
-                            <div class="frame-34-Msb">
+                            <div className="frame-34-Msb">
                               {/* <img
-                                class="currency-coin-dollar-UhK"
+                                className="currency-coin-dollar-UhK"
                                 src="REPLACE_IMAGE:48:1878"
                               /> */}
-                              <div class="item-10--wqo">공과금 10% 할인</div>
+                              <div className="item-10--wqo">
+                                공과금 10% 할인
+                              </div>
                             </div>
-                            <div class="frame-27-T3T">
+                            <div className="frame-27-T3T">
                               {/* <img
-                                class="note-02-zJH"
+                                className="note-02-zJH"
                                 src="REPLACE_IMAGE:48:1894"
                               /> */}
-                              <div class="item-5--uRF">영화 5% 할인</div>
+                              <div className="item-5--uRF">영화 5% 할인</div>
                             </div>
                           </div>
                         </div>
                       </div>
-                      <div class="frame-23-cKf">
-                        <p class="learn-more-Kjs">Learn More</p>
-                        {/* <img class="vector-7-SpV" src="REPLACE_IMAGE:191:709" /> */}
+                      <div className="frame-23-cKf">
+                        <p className="learn-more-Kjs">Learn More</p>
+                        {/* <img className="vector-7-SpV" src="REPLACE_IMAGE:191:709" /> */}
                       </div>
                     </div>
                   </div>
-                  <div class="group-1000003059-tgV">
-                    {/* <img class="icon-RRX" src="REPLACE_IMAGE:42:1722" /> */}
+                  <div className="group-1000003059-tgV">
+                    {/* <img className="icon-RRX" src="REPLACE_IMAGE:42:1722" /> */}
                   </div>
                 </div>
               </div>
             </div>
-            {/* <img class="star-img-i9j" src="REPLACE_IMAGE:13:2330" /> */}
+            {/* <img className="star-img-i9j" src="REPLACE_IMAGE:13:2330" /> */}
           </div>
         </div>
-      </div>
-    </div>
+      </ShareMain>
+    </ShareBack>
   );
 }
 
