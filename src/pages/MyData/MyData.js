cd641c9438ba2908ad91503bae6e372096b379db
ksweeni <77386337+ksweeni@users.noreply.github.com>
2024-02-20 17:53:34 +0900

    Feat: chartJs 제외 데이터 연동

--- src/pages/MyData/MyData.js
+++ src/pages/MyData/MyData.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CardDataList,
   CardInfoDesc,
@@ -42,16 +42,49 @@ import {
 } from "../../components/MyDataStyle/MyDataChart";
 
 function MyData(props) {
+  const [myAvg, setmyAvg] = useState("");
+  const [myCardForMonth, setmyCardForMonth] = useState([]);
+  const [myCategoryView, setmyCategoryView] = useState([]);
+  const [myCompare, setmyCompare] = useState([]);
+  const [myLimit, setmyLimit] = useState([]);
+  const [myNextLevel, setmyNextLevel] = useState([]);
+
   const options = [
     { value: "Newest", label: "Sort by Newest" },
     { value: "Highest", label: "Sort by Highest" },
   ];
-  axios
-    .get("/getMyLimit.do")
-    .then(function (res) {
-      console.log(res.data);
-    })
-    .catch(function (error) {});
+  useEffect(() => {
+    axios
+      .post("/getMyData.do", {
+        memberId: "user2",
+      })
+      .then((res) => {
+        console.log(res.data);
+        setmyAvg(res.data.myAvg);
+        setmyCardForMonth(res.data.myCardForMonth);
+        setmyCategoryView(res.data.myCategoryView);
+        setmyCompare(res.data.myCompare);
+        setmyLimit(res.data.myLimit);
+        setmyNextLevel(res.data.myNextLevel);
+      })
+      .catch(function (error) {});
+  }, []);
+
+  const CardDataListWrapper = ({ myCardForMonth }) => {
+    return (
+      <CardDataList>
+        {myCardForMonth &&
+          myCardForMonth.map((card, index) => (
+            <CardInfoDesc key={index}>
+              <CardSemiPic image={blackvelvet} />
+              <CardName>{card.byCardName}</CardName>
+              <Price>{new Intl.NumberFormat().format(card.point)}</Price>
+            </CardInfoDesc>
+          ))}
+      </CardDataList>
+    );
+  };
+
   return (
     <MyDataBack>
       <HeaderLogoutBtn />
@@ -59,37 +92,40 @@ function MyData(props) {
       <LimitWrapper isFirst>
         <LimitDiv>
           <DataTitle>한도현황</DataTitle>
-          <DataView>150,000</DataView>
-          <DataDesc>남은 금액 110,000</DataDesc>
+          <DataView>{myLimit.limitedAmount}</DataView>
+          <DataDesc>남은 금액 {myLimit.historyAmount}</DataDesc>
           <CardLimitPic image={blackvelvet} />
         </LimitDiv>
         <LimitDiv>
-          <DataTitle>평균사용량</DataTitle> <DataView>150,000</DataView>
-          <DataDesc>1월까지의 평균 사용 금액</DataDesc>
+          <DataTitle>평균사용량</DataTitle> <DataView>{myAvg}</DataView>
+          <DataDesc>2월까지의 평균 사용 금액</DataDesc>
           <LevelDiv>
             <LevelPic level={bronze} />
-            <LevelTitle>bronze</LevelTitle>
+            <LevelTitle>{myNextLevel.memberClass}</LevelTitle>
           </LevelDiv>
         </LimitDiv>
         <LimitDiv>
-          <ToNext>20,0000</ToNext>
-          <ToNextSub>Silver까지 남은 금액</ToNextSub>
+          <ToNext>{myNextLevel.toNextClass}</ToNext>
+          <ToNextSub>{myNextLevel.nextClass}까지 남은 금액</ToNextSub>
 
           <ForNow>
-            현재 사용 금액 <span>50,000</span>
+            현재 사용 금액 <span>{myNextLevel.priceHasUsed}</span>
           </ForNow>
         </LimitDiv>
       </LimitWrapper>
       <LimitWrapper>
         <SaleForMonth>
           <MonthHeading>
-            <DataTitle>월 별 카드 할인 금액</DataTitle>
+            <DataTitle>By 카드 포인트 현황</DataTitle>
             <Sorting>
               <Select options={options} />
             </Sorting>
           </MonthHeading>
 
-          <CardDataList>
+          <CardDataListWrapper myCardForMonth={myCardForMonth} />
+          {/*         
+        <CardDataList>
+         
             <CardInfoDesc>
               <CardSemiPic image={blackvelvet} />
               <CardName>The Blue</CardName>
@@ -143,11 +179,11 @@ function MyData(props) {
               <CardName>The Blue</CardName>
               <Price>210,000</Price>
             </CardInfoDesc>
-          </CardDataList>
+          </CardDataList> */}
         </SaleForMonth>
         <CompareLastMonth>
           <DataDesc>저번달 VS 이번 달</DataDesc>
-          <DataView>150,000 사용했습니다</DataView>
+          <DataView>{myCompare.moreThanUsed} 사용했습니다</DataView>
           <LineChart></LineChart>
         </CompareLastMonth>
       </LimitWrapper>
