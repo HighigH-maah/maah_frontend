a43dbce2f684127521243a8cb508a739f2e84934
ksweeni <77386337+ksweeni@users.noreply.github.com>
2024-02-23 09:16:17 +0900

    Feat: 최고 레벨 처리

--- src/components/MyDataStyle/MyDataChart.js
+++ src/components/MyDataStyle/MyDataChart.js
@@ -43,24 +43,6 @@ export const options = {
 };
 const labels = ["1일", "15일", "30일"]; //x축 기준
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "분류 1", //그래프 분류되는 항목
-      data: [1, 2, 3, 4, 5, 6, 7], //실제 그려지는 데이터(Y축 숫자)
-      borderColor: "rgb(255, 99, 132)", //그래프 선 color
-      backgroundColor: "rgba(255, 99, 132, 0.5)", //마우스 호버시 나타나는 분류네모 표시 bg
-    },
-    {
-      label: "분류 2",
-      data: [2, 3, 4, 5, 4, 7, 8],
-      borderColor: "rgb(53, 162, 235)", //실제 그려지는 데이터(Y축 숫자)
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
-
 export const contentInner = styled.div`
   width: 470px;
   height: 297px;
@@ -169,7 +151,6 @@ export function MyDoughnutChart({ myCategory }) {
   const chartRef = useRef(null);
 
   useEffect(() => {
-    // Update the chart's gradient when the component mounts or data changes
     if (chartRef.current && chartRef.current.chartInstance) {
       const chartInstance = chartRef.current.chartInstance;
       const gradients = chartData.datasets[0].backgroundColor;
@@ -189,10 +170,7 @@ export function MyDoughnutChart({ myCategory }) {
       <PercentZone>
         {topCategories.map((category, index) => (
           <PercentBox key={index}>
-            <Ptitle>
-              {/* Add your SVG here or customize as needed */}
-              {category.benefitName}
-            </Ptitle>
+            <Ptitle>{category.benefitName}</Ptitle>
 
             <PData>
               {new Intl.NumberFormat().format(category.benefitData)}
